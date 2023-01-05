55a9992dbfe838f3137d4d7545d284b320b54c4e
Jeon Eonseok <jseokeon@gmail.com>
2023-01-05 22:35:29 +0900

    feat: valid input with red box

--- src/component/Input.js
+++ src/component/Input.js
@@ -6,9 +6,16 @@ import classes from './Input.module.css';
 const Input = (props) => {
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onChange(userName, userAge);
-    setUserName('');
-    setUserAge('');
+    if (!userName) {
+      setInValidName(true);
+    }
+    if (!userAge) {
+      setInValidAge(true);
+    } else if (userName && userAge) {
+      props.onChange(userName, userAge);
+      setUserName('');
+      setUserAge('');
+    }
   };
 
   const [userName, setUserName] = useState('');
@@ -16,33 +23,47 @@ const Input = (props) => {
 
   const nameChangeHandler = (event) => {
     setUserName(event.target.value);
+    setInValidName('');
   };
   const ageChangeHandler = (event) => {
     setUserAge(event.target.value);
+    setInValidAge('');
   };
+  const [inValidName, setInValidName] = useState(false);
+  const [inValidAge, setInValidAge] = useState(false);
 
   return (
+    // TODO: cancel
     <Card className={classes.wrapper__head}>
       <form onSubmit={submitHandler}>
         <div className={classes.head__inputs}>
-          <div className={classes.head__input}>
+          {/* TODO: delete repeat (extra study) */}
+          <div
+            className={`${classes.head__input} ${
+              inValidName && classes.inValidName
+            }`}
+          >
             <label htmlFor='userName'>User Name</label>
             <input
               value={userName}
               id='userName'
               type='text'
+              // required
               onChange={nameChangeHandler}
-              required
             ></input>
           </div>
-          <div className={classes.head__input}>
+          <div
+            className={`${classes.head__input} ${
+              inValidAge && classes.inValidAge
+            }`}
+          >
             <label htmlFor='userAge'>User Age</label>
             <input
               value={userAge}
               id='userAge'
               type='number'
               min='0'
-              required
+              // required
               onChange={ageChangeHandler}
             ></input>
           </div>
