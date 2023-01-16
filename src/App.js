8fb08221861c60586b59dcb4e1800203a50eb3c9
Jeon Eonseok <jseokeon@gmail.com>
2023-01-17 08:38:50 +0900

    reset

--- src/App.js
+++ src/App.js
@@ -1,47 +1,5 @@
-import React, { useState } from 'react';
-import Card from './UI/Card';
-import Input from './component/Input';
-import UserList from './component/UserList';
-
 const App = () => {
-  const [userData, setUserData] = useState([]);
-  const onChangeHandler = (userName, userAge) => {
-    setUserData((prevData) => {
-      return [
-        ...prevData,
-        { name: userName, age: userAge, id: Math.random().toString() },
-      ];
-    });
-  };
-  const deleteHandler = (getID) => {
-    setUserData((prevData) => {
-      return prevData.filter((elem) => elem.id !== getID);
-    });
-  };
-
-  let content = (
-    <Card>
-      <p style={{ textAlign: 'center' }}>No Data</p>
-    </Card>
-  );
-  if (userData.length > 0) {
-    content = <UserList userData={userData} onClick={deleteHandler} />;
-  }
-
-  return (
-    <div>
-      <Input onChange={onChangeHandler} />
-      {content}
-      {/* {userData.length === 0 && (
-        <Card>
-          <p style={{ textAlign: 'center' }}>No Data</p>
-        </Card>
-      )}
-      {userData.length > 0 && (
-        <UserList userData={userData} onClick={deleteHandler} />
-      )} */}
-    </div>
-  );
+  return <div>?</div>;
 };
 
 export default App;
