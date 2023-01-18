3782aea4b2aad68b1aa03a7b320b49dbc4b20075
Jeon Eonseok <jseokeon@gmail.com>
2023-01-19 00:18:33 +0900

    RESET

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 
 const App = () => {
-  return (
-    <React.Fragment>
-      <div>123</div>
-    </React.Fragment>
-  );
+  return;
 };
 
 export default App;
