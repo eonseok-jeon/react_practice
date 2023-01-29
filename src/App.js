39528e51890f25f754c1b2d0c4d2227d03163739
Jeon Eonseok <jseokeon@gmail.com>
2023-01-29 09:13:56 +0900

    ??

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
+  const toggle = useSelector((state) => state.toggle.toggle);
   return (
     <Layout>
-      <Cart />
+      {toggle && <Cart />}
       <Products />
     </Layout>
   );
